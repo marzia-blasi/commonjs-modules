479a8acb2eb390568ef23ffaf425a1300aab1e0b
marzia <you@example.com>
2025-06-25 16:19:03 +0200

    Implement activities function in hobbies.js and update main.js to log activities

--- hobbies.js
+++ hobbies.js
@@ -1,3 +1,12 @@
 /**2 - hobbies.js
 Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
 Esporta la funzione dal file.  */
+
+// return arrayHobbies = ["hobbyOne", "hobbyTwo", "hobbyThree"];
+function activities(hobbyOne, hobbyTwo, hobbyThree) {
+  return (arrayHobbies = [hobbyOne, hobbyTwo, hobbyThree]);
+}
+
+//console.log(activities("nuoto", "golf", "calcio"));
+
+module.exports = activities;
--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const person = require("./names");
+const activities = require("./hobbies");
+
 console.log(person("davide", "rosso"));
+
+console.log(activities("leggere", "mangiare", "giocare"));
