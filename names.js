df0e5c542d6d69b249e931476d35fdb55b60a957
marzia <you@example.com>
2025-06-25 15:39:07 +0200

    Add initial HTML structure and implement names.js functionality

---
+++ names.js
@@ -0,0 +1,13 @@
+/* 1 - names.js
+Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+Esporta la funzione dal file.
+* */
+
+function person(firstName, lastName) {
+  return (objName = {
+    nome: firstName,
+    cognome: lastName,
+  });
+}
+
+console.log(person("marzia", "blasi"));
